b4d58fd07088d0ce2c7515863c4c7cae3cd7c0da
Daniel Caldera <bdcaldera@bdcaldera.com>
2020-04-14 05:07:52 -0500

    Added Mutation for Like Button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-const App = () => (
-    <>
-        <Logo />
-        <GlobalStyles />
-        <ListOfCategories />
-        <ListOfPhotoCards />
-    </>
-)
+const App = () => {
+    const urlParams = new window.URLSearchParams(window.location.search)
+    const detailId = urlParams.get('detail')
+
+    return (
+        <>
+            <GlobalStyles />
+            <Logo />
+            {detailId ? (
+                <PhotoCardWithQuery id={detailId} />
+            ) : (
+                <Fragment>
+                    <ListOfCategories />
+                    <ListOfPhotoCards categoryId={1} />
+                </Fragment>
+            )}
+        </>
+    )
+}
 
 export default App
