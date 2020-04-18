43152ea676f5b21e37d8a6208d35f85a9a88a765
Daniel Caldera <bdcaldera@bdcaldera.com>
2020-04-18 03:15:08 -0500

    Added LikePhoto Implementation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,25 @@ import { ApolloProvider } from 'react-apollo'
 import Context from './Context'
 import App from './App'
 
-const client = new ApolloClient({ uri: 'https://petgram-back-00-daniel.now.sh/graphql' })
+const client = new ApolloClient({
+    uri: 'https://petgram-back-00-daniel.now.sh/graphql',
+    request: (operation) => {
+        const token = window.sessionStorage.getItem('token')
+        const authorization = token ? `Bearer ${token}` : ''
+        operation.setContext({
+            headers: {
+                authorization,
+            },
+        })
+    },
+    onError: (error) => {
+        const { networkError } = error
+        if (networkError && networkError.result.code === 'invalid_token') {
+            window.sessionStorage.removeItem('token')
+            window.location.href = '/'
+        }
+    },
+})
 
 ReactDOM.render(
     <Context.Provider>
